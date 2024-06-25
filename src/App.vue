f24198c3c4b68ab55819100a4eec7bb3a4b222a3
Songkyeok <kihyeoksong0730@gmail.com>
2024-06-25 14:21:52 +0900

    id, pw 찾기, 마이페이지 공통 추가

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,12 @@
 <script>
 import Header from "./layouts/header.vue";
 import Footer from "./layouts/footer.vue";
-import MypageLayout from "./layouts/mypageLayout.vue";
-import MypageSidebar from "./layouts/mypageSideBar.vue";
 
 export default {
   name: 'App',
   components: {
     Header,
     Footer,
-    MypageLayout,
-    MypageSidebar
   }
 }
 </script>
--- src/mypages/Profile.vue
+++ src/mypages/Profile.vue
@@ -1,25 +1,12 @@
 <template>
-    <MypageSidebar />
 
-    <div class="profile_container">
-        
-    </div>
 </template>
 <script>
-import MypageLayout from "../layouts/mypageLayout.vue";
-import MypageSidebar from "../layouts/mypageSideBar.vue";
 
 export default {
-    components: {
-        MypageLayout,
-        MypageSidebar
-    }
 }
 </script>
 
 <style scoped>
-.profile_container {
-    width: 1080px;
-    background: coral;
-}
+
 </style>
