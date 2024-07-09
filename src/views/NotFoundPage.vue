7275ca564cde2bd29902e464318e8bf24a577b6a
Songkyeok <kihyeoksong0730@gmail.com>
2024-07-09 12:51:55 +0900

    404 페이지 이전 페이지 수정

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -15,7 +15,7 @@
         <div class="error__title">404</div>
         <div class="error__subtitle">Hmmm...</div>
         <div class="error__description">It looks like one of the  developers fell asleep</div>
-            <button class="error__button error__button--active" @click="this.$router.push({ path: '/' });">Back</button>
+            <button class="error__button error__button--active" @click="this.$router.go(-1);">Back</button>
             <!-- <button class="error__button">CONTACT</button> -->
         </div>
 
