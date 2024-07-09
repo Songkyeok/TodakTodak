8a29bab91f0f866e702ecccdbf5ae1369e11ca7a
Songkyeok <kihyeoksong0730@gmail.com>
2024-07-09 12:44:28 +0900

    공통 라우터 전체 수정, 404페이지 추가

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
-  <Header />
   <router-view />
-  <Footer />
 </template>
 
 <script>
-import Header from "./layouts/header.vue";
-import Footer from "./layouts/footer.vue";
 
 export default {
   name: 'App',
-  components: {
-    Header,
-    Footer,
-  }
 }
 </script>
